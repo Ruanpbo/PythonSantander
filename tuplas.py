76d27612794a4fef8edfad13523a2be1c747f320
Ruan Pablo <64712177+Ruanpbo@users.noreply.github.com>
2024-10-10 19:00:31 +0000

    Terceira aula

---
+++ tuplas.py
@@ -0,0 +1,15 @@
+# Tuplas
+#
+# Uma tupla é uma estrutura de dados imutavel e ordenada que permite armazenar uma coleção de elementos.
+# Os elementos de uma tupla são encerrados entre parênteses (), separado por virgulas.
+#
+# Criação e acesso
+# Para criar uma tupla, encerre os elementos entre parenteses:
+ponto = (3,4)
+
+# Para acessar os elementos de uma tupla, utilize o indice do elemento entre colchetes, similar as listas:
+print(ponto[0]) # Imprime 3
+print(ponto[1]) # Imprime 4
+
+# Métodos de tuplas
+#   Embora as tuplas sejam imutaveis, Python oferece vários métodos úteis para trabalhar com elas 
