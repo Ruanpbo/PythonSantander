d08c21888e39252dc6d7d86553a69756cc39cf32
Ruan Pablo <64712177+Ruanpbo@users.noreply.github.com>
2024-10-11 14:51:51 +0000

    git

--- tuplas.py
+++ tuplas.py
@@ -12,4 +12,13 @@ print(ponto[0]) # Imprime 3
 print(ponto[1]) # Imprime 4
 
 # Métodos de tuplas
-#   Embora as tuplas sejam imutaveis, Python oferece vários métodos úteis para trabalhar com elas 
+#   Embora as tuplas sejam imutaveis, Python oferece vários métodos úteis para trabalhar com elas
+#
+#   count(elemento): devolve o número de vezes que um elemento aparece na tupla
+#   index(elemento): devolve o índice da primeira aparição de um elemento na tupla. Opcionalmente, pode-se especificar o início e fim da busca
+#   len(tupla): embora não seja um método de tupla propriamente dito, esta função incorporada devolve o comprimento da tupla.
+minha_tupla = (1,2,3,2,4,2)
+
+print(minha_tupla.index(2)) #saida: 1
+print(minha_tupla.index(2, 2)) #saida: 3
+print(minha_tupla.index(2,2,4)) #saida: 3
