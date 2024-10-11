317ef85fbf368ec40fc51e7ccf622b012d230d95
Ruan Pablo <64712177+Ruanpbo@users.noreply.github.com>
2024-10-11 17:24:21 +0000

    Adicionando metodos no dicionario.py

--- dicionarios.py
+++ dicionarios.py
@@ -12,4 +12,16 @@ print(pessoa["cidade"]) # Imprime "Madri"
 #
 # Métodos de dicionários
 # Os dicionario em python têm varios métodos incorporados para manipular e acessar os elementos. Alguns metodos comuns sao:
-#   keys(): 
+#   keys(): retorna uma visualização de todas as chaves do dicionario
+#   values(): retorna uma visualização de todos os valores do dicionario
+#   items(): retorna uma visualização de todos os pares chave-valor do dicionario
+#   update(outro_dicionario): atualiza o dicionario com os pares chaves-valor de outro dicionario
+
+pessoa = {"nome":"Joao","idade":25,"cidade":"Madri"}
+
+print(pessoa.keys()) # Imprime dict_keys(["nome","idade","cidade"])
+print(pessoa.values()) # Imprime dic_value(["João",25,"Madri"])
+print(pessoa.items()) # Imprime dict_items([("nome","João"),("idade",25),("cidade","Madri")])
+
+pessoa.update({"profissao": "Engenheiro"}) # Atualiza o dicionario pessoa adicionando a chave profissao e o valor engenheiro
+print(pessoa) # Imprime {"nome": "João", "idade":25, "cidade": "Madri", "profissao": "Engenheiro"}
