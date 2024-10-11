dcf9a1696c05541aca5f2e466d4e1acf156e18d1
Ruan Pablo <64712177+Ruanpbo@users.noreply.github.com>
2024-10-11 15:07:52 +0000

    Adicionando dicionarios.py ao projeto

---
+++ dicionarios.py
@@ -0,0 +1,15 @@
+# Criação e acesso
+#
+# Para criar um dicionário, utilize chaves e separe as chaves e valores com dois pontos.
+pessoa = {"nome":"Joao","idade":25,"cidade":"madri"}
+
+# Para acessar os valores de um dicionario, utilize a chave correspondente entre cochetes:
+print(pessoa["nome"]) # Imprime "Joao"
+print(pessoa["idade"]) # Imprime 25
+print(pessoa["cidade"]) # Imprime "Madri"
+
+# Você também podem utilizar o método get() para obter o valor de uma chave
+#
+# Métodos de dicionários
+# Os dicionario em python têm varios métodos incorporados para manipular e acessar os elementos. Alguns metodos comuns sao:
+#   keys(): 
